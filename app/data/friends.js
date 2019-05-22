5fb6edeed2084aee35bf5a771ef6279b60a8cae8
Carlos Quinto <carlquin77@gmail.com>
2019-05-22 14:15:39 -0700

    Added some friends data

---
+++ app/data/friends.js
@@ -0,0 +1,34 @@
+[
+    {
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "Rocky",
+        "photo": "https://www.muscleandfitness.com/sites/muscleandfitness.com/mensfitness/files/_main_rocky_0.jpg",
+        "scores": [
+            4,
+            4,
+            8,
+            2,
+            5,
+            6,
+            4,
+            9,
+            9,
+            1
+        ]
+    }
+]
